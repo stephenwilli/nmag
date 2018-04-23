9543f35bed85d173e441e27f2e6531cdf8433301
Stephen Williams <stephen@wearetmbr.com>
2018-04-23 13:07:17 -0600

    blast modal

---
+++ wp-content/themes/simplemag-child/inc/modal.php
@@ -0,0 +1,10 @@
+<!-- NBLAST MODAL -->
+<div style="display: none;">
+  <div id="modal-signup" class="nblast-">
+    <h2><img src="/wp-content/uploads/2015/02/nblast_01.jpg" alt="" width="100px " /></h2>
+    <h2>NBlast Signup</h2>
+    Sign up for N Magazine’s weekly e-newsletter and stay up to date on what’s hot, hip, and happening on Nantucket!
+
+    <?php gravity_form(1, false, false, false, '', true); ?>
+  </div>
+</div>
--- wp-content/themes/simplemag-child/js/main.js
+++ wp-content/themes/simplemag-child/js/main.js
@@ -3,60 +3,34 @@ jQuery(document).ready(function($) {
  	$("a#nblast-modal").fancybox({
 		'hideOnContentClick': true
 	});
+  
+  // Toggle the modal open - close
+  function toggleEmailModal()
+  {
+    $.fancybox.open('#modal-signup');
+  }	  
 
-	// $(window).on('resize', function () {
- //    $('.header-default').toggleClass('mobile-header', $(window).width() < 750);
-	// });
+  // If user has not completed the email form
+  if (! jQuery.cookie('email-complete')) {
 
-	// $(window).resize(function(){
- //    $('div.et_lb_module.et_lb_column.et_lb_resizable').width(
- //        window.matchMedia('all and (max-width: 786px)') ? '100%' : '20%'
- //    );
-	// });
+    // set page count if net set
+    if (! jQuery.cookie('email-page')) {
+      jQuery.cookie('email-page', '1', {  path: '/' } );
+    } else {
+      var value = parseFloat(jQuery.cookie('email-page'));
+      value = value + 1;
+      jQuery.cookie('email-page', value, {  path: '/' } );
+    }
+
+    // If the have viewed 3 pages and not closed the modal in 7 days
+    if (jQuery.cookie('email-page') === '3' && !jQuery.cookie('close-email-page') ) {
+      toggleEmailModal();
+    }
+    
+    // listen if they close modal and set the cookie
+    jQuery( "#gform_submit_button_1, .fancybox-close, .fancybox-overlay" ).click(function() {
+      jQuery.cookie('close-email-page', 'true', { expires: 7, path: '/' } );
+    });
+}	
 
 });	
-
-jQuery(function() {
-
-	// Toggle the modal open - close
-	function toggleEmailModal()
-	{
-		jQuery( ".overlay" ).fadeToggle( "slow", "linear" );
-		jQuery( ".modal" ).fadeToggle( "slow", "linear" );
-	}	  
-
-	// If user has not completed the email form
-	if (! jQuery.cookie('email-complete')) {
-
-		// set page count if net set
-		if (! jQuery.cookie('email-page')) {
-			jQuery.cookie('email-page', '1', {  path: '/' } );
-		} else {
-			var value = parseFloat(jQuery.cookie('email-page'));
-			value = value + 1;
-			jQuery.cookie('email-page', value, {  path: '/' } );
-		}
-
-		// If the have viewed 3 pages and not closed the modal in 7 days
-		if (jQuery.cookie('email-page') === '3' && !jQuery.cookie('close-email-page') ) {
-			toggleEmailModal();
-		}
-		
-		// listen if they close modal and set the cookie
-		jQuery( ".overlay, .close-email" ).click(function() {
-			toggleEmailModal()
-			jQuery.cookie('close-email-page', 'true', { expires: 7, path: '/' } );
-		});
-		
-		// Push event to analytics - Universal Analytics code
-		//jQuery('#gform_1').on('submit', function() {
-			//ga('send', 'event', 'Newsletter Sign Up', 'Submitted', 'All Pages And Pop up');
-		//});
-		//jQuery('#gform_2').on('submit', function() {
-			//ga('send', 'event', 'Contact form', 'Submitted', 'Contact Page Info request');
-		//});
-
-		//jQuery('.nletter_signup').on('click', toggleEmailModal);
-
-}	
-});
