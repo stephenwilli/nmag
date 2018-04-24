fc36e9ad3cd5b2e0ae6ec8bac40bd9b32d80752e
Stephen Williams <stephen@wearetmbr.com>
2018-04-23 18:25:42 -0600

    staging site

--- wp-content/themes/simplemag-child/inc/modal.php
+++ wp-content/themes/simplemag-child/inc/modal.php
@@ -1,7 +1,7 @@
 <!-- NBLAST MODAL -->
 <div style="display: none;">
   <div id="modal-signup" class="nblast-">
-    <h2><img src="/wp-content/uploads/2015/03/logo_trans.png" alt="" /></h2>
+    <h2><img src="/wp-content/uploads/2018/04/logo_01.png" alt="" /></h2>
     <h2>NBlast Signup</h2>
     <p>Sign up for N Magazine’s weekly e-newsletter and stay up to date on what’s hot, hip, and happening on Nantucket!</p>
 
